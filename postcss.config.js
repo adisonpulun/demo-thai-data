c3344ea1132a71617d271f9dd5857c6820d6af9d
niawjunior <pasupol@dome.cloud>
2020-03-30 21:28:59 +0700

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
-const tailwindcss = require('tailwindcss');
+const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+
 module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
-    ],
+  plugins: [
+    require("autoprefixer"),
+    tailwindcss("./tailwind.js"),
+    process.env.NODE_ENV === "production" &&
+      purgecss({
+        content: ["./src/**/*.js"],
+        css: ["./src/**/*.css"],
+      }),
+  ],
 };
